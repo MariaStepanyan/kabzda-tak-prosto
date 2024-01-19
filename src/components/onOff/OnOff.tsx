6d863fdd145240df833cb7d800e105ece18d43a9
MariaStepanyan <drmariastepanyan@gmail.com>
2024-01-19 21:52:17 +0300

    OnOff onClick

--- src/components/onOff/OnOff.tsx
+++ src/components/onOff/OnOff.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import './onoff.css'
+import React, { useState } from 'react'
+import s from './onoff.module.css'
 
 type OnOffPropsType = {
   isOn: boolean
 }
 
 export const OnOff = (props: OnOffPropsType) => {
+  const [on, setOn] = useState(false)
+  const onClickOnHandler = () => {
+    setOn(true)
+  }
+
+  const onClickOffHandler = () => {
+    setOn(false)
+  }
   return (
-    <div className="wrapper">
-      <button className={props.isOn ? 'on' : ''}>on</button>
-      <button className={props.isOn ? '' : 'off'}>off</button>
-      {props.isOn && <span className="on dot"></span>}
-      {!props.isOn && <span className="off dot"></span>}
-      {/* <span className={props.isOn ? 'dot on' : 'dot off'}></span> */}
+    <div className={s.wrapper}>
+      <button onClick={onClickOnHandler} className={on ? s.on : ''}>
+        on
+      </button>
+      <button onClick={onClickOffHandler} className={on ? '' : s.off}>
+        off
+      </button>
+      <span className={`${s.dot} ${on ? s.on : s.off}`}></span>
+      {/* {on && <span className={`${s.on} ${s.dot}`}></span>}
+      {!on && <span className={`${s.off} ${s.dot}`}></span>} */}
     </div>
+
+    //   <div className="wrapper">
+    //   <button className={props.isOn ? 'on' : ''}>on</button>
+    //   <button className={props.isOn ? '' : 'off'}>off</button>
+    //   {props.isOn && <span className="on dot"></span>}
+    //   {!props.isOn && <span className="off dot"></span>}
+    //   {/* <span className={props.isOn ? 'dot on' : 'dot off'}></span> */}
+    // </div>
   )
 }
